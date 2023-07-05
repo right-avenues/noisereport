f4eaa718acc297aad06c6787f32350991dacba79
between0n1 <sunhyeok.jung@gmail.com>
2023-07-04 21:31:09 -0400

    color

--- index.js
+++ index.js
@@ -24,14 +24,46 @@ $(document).ready(function () {
             `https://rightavenues-free-noise-report.s3.amazonaws.com/zipcode-noise-stats-v2/${zipcode.value}.json`
           );
           const json = await response.json();
+          const percentile = json[`stats.${type}_v1_category`];
           TYPES.map((type) => {
             document.getElementById(
               `noisereport_${type}_complaints`
             ).innerText = json[`stats.${type}`] + " noise complaints";
 
-            document.getElementById(
-              `noisereport_${type}_percentile`
-            ).innerText = json[`stats.${type}_v1_category`];
+            switch (percentile) {
+              case "other":
+                document.getElementById(
+                  `noisereport_${type}_percentile`
+                ).innerText = "Below 25 percentile";
+                document.getElementById(
+                  `noisereport_${type}_circle`
+                ).style.backgroundColor = "yellow";
+
+                // yellow
+                break;
+              case "p90":
+                document.getElementById(
+                  `noisereport_${type}_percentile`
+                ).innerText = "Top 10 percentile.";
+                document.getElementById(
+                  `noisereport_${type}_circle`
+                ).style.backgroundColor = "red";
+                // red
+                break;
+              case "p75":
+                document.getElementById(
+                  `noisereport_${type}_percentile`
+                ).innerText = "Top 25 percentile.";
+                document.getElementById(
+                  `noisereport_${type}_circle`
+                ).style.backgroundColor = "orange";
+                // orange
+
+                break;
+
+              default:
+                break;
+            }
           });
           console.log(json);
         } catch (error) {
@@ -47,3 +79,26 @@ $(document).ready(function () {
 // https://cdn.jsdelivr.net/gh/right-avenues/noisereport@a69c895/index.js
 // https://cdn.jsdelivr.net/gh/right-avenues/noisereport@a69c895/index.js
 // https://raw.githubusercontent.com/right-avenues/noisereport/main/index.js
+
+const sample = {
+  zip_code: "10020",
+  "stats.construction": 11.0,
+  "stats.music_party": 19.0,
+  "stats.other_street_noise": 10.0,
+  "stats.vehicle": 3.0,
+  "stats.helicopter": 0.0,
+  "stats.construction_area_normalized": 9.0470379369,
+  "stats.music_party_area_normalized": 15.6267018911,
+  "stats.other_street_noise_area_normalized": 8.2245799427,
+  "stats.vehicle_area_normalized": 2.4673739828,
+  "stats.helicopter_area_normalized": 0.0,
+  "stats.construction_v1_category": "other",
+  "stats.music_party_v1_category": "other",
+  "stats.other_street_noise_v1_category": "other",
+  "stats.vehicle_v1_category": "other",
+  "stats.helicopter_v1_category": "other",
+  fdny_station_count: 0.0,
+  hospital_count: 0.0,
+  emergency_services_count: 0.0,
+  emergency_services_count_v1_category: "other",
+};
