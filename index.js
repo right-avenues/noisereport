df3cf0fbc81bac611da1dfb0a78395d86ede498b
between0n1 <sunhyeok.jung@gmail.com>
2023-07-04 20:19:49 -0400

    ASD

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log("VSC");
+$(document).ready(function () {
+  console.log("VSC");
+});
 
 // https://cdn.jsdelivr.net/gh/right-avenues/noisereport@a69c895/index.js
 // https://cdn.jsdelivr.net/gh/right-avenues/noisereport@a69c895/index.js
+// https://raw.githubusercontent.com/right-avenues/noisereport/main/index.js
